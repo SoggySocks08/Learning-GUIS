02a8e82d1c7111324bb57d5490f1cd4192a0c55a
SoggySocks08 <devdog0708@gmail.com>
2026-03-10 16:23:22 -0700

    Update GUImenuV2.py
    Order shown on bottom as it is building, terminal clears before running

--- GUImenuV2.py
+++ GUImenuV2.py
@@ -1,45 +1,47 @@
 import os, time
+clear = 'cls' if os.name == 'nt' else 'clear'
+os.system(clear)
 from tkinter import *
 
 class App:
-# ===================
-# Constructor
-# ===================
+  # ===================
+  # Constructor
+  # ===================
   def __init__(self, root):
     self.root = root
     self.order=[]
-    self.setup_window()
-    self.create_widgets()
     self.cust_Ords = {}
     self.menu_dict = {"chicken sandwich": 5,
             "spicy sandwich": 8,
             "fries": 5,
             "da bev": 4,
             "salad": 4,
-            "shake":5
-  }
-
+            "shake":5}
+           
     self.inventory_dict = {"chicken sandwich": 5,
              "spicy sandwich": 5,
              "fries": 5,
              "da bev": 5,
              "salad": 5,
              "shake": 5}
-    
+   
     self.restocks_dict = {"chicken sandwich": 0,
              "spicy sandwich": 0,
              "fries": 0,
              "da bev": 0,
              "salad": 0,
              "shake": 0}
-             
-# ===================
-# Setup Methods
-# ===================
+    
+    self.letterBank=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    self.setup_window()
+    self.create_widgets()
+  # ===================
+  # Setup Methods
+  # ===================
   def setup_window(self):
     self.root.title("Chick-Fil-A")
-    self.root.geometry('500x250')
-  
+    self.root.geometry('750x750')
+ 
   def create_widgets(self):
 
     self.create_frames()
@@ -50,60 +52,65 @@ class App:
     self.create_frame2_buttons()
     self.create_frame2_labels()
     self.create_frame2_entry()
+   
+    self.create_frame3_buttons()
 
     self.frame2.grid_remove()
+    self.frame3.grid_remove()
 
   def create_frames(self):
-# frame1 for main order page
+    # frame1 for main order page
     self.frame1 = Frame(self.root)
     self.frame1.grid(row=0,column=0)
 
-# frame2 for name entry
+    # frame2 for name entry
     self.frame2 = Frame(self.root)
     self.frame2.grid(row=0,column=0)
+   
+    #frame3 for either continuing the order
+    self.frame3 = Frame(self.root)
+    self.frame2.grid(row=0,column=0)
 
+ # First order screen
+  
   def create_frame1_buttons(self):
-# dictionary contains all button objects corresponding to each menu item
+    # dictionary contains all button objects corresponding to each menu item
     items = ["Chicken Sandwich", "Spicy Sandwich", "Fries", "Salad", "Da Bev","Shake"]
-# r = rows, c = columns. Used to place the buttons in the correct place on grid in for loop below
+    # r = rows, c = columns. Used to place the buttons in the correct place on grid in for loop below
     r = 0
     c = 0
-# for loop creates all menu buttons automatically, 3 per column
+    # for loop creates all menu buttons automatically, 3 per column
     for item in items:
       btn = Button(self.frame1,
-                   text=item,
-                   width=15,
+                   text=f"{item}: ${self.menu_dict[item.lower()]}",
+                   width=20,
                    command=lambda item=item: self.add2Order(item))
-      btn.grid(row=r,column=c)
+      btn.grid(row=r,column=c, sticky=W)
       if r == 2:
         c +=1
         r=0
         continue
       r+=1
 
-# Submit button (seperate from menu item buttons)
+    # Submit button (seperate from menu item buttons)
     self.submit=Button(self.frame1,
                        text="Submit",
-                       width=15,
+                       width=20,
                        height=2,
                        command=lambda: self.submitFunc()
     )
-    self.submit.grid(row=4,column=0, pady=25)
-
-# End Order Button (Seperate from all other buttons)
-    self.endOrderbtn = Button(self.frame1,
-                              text="All Done!",
-                              width=15,
-                              height=2,
-                              command=self.endOrder)
-    self.endOrderbtn.grid(row=4,column=1, pady=25)
-  
+    self.submit.grid(row=4,column=0, pady=25, sticky=W)
+ 
   def create_frame1_labels(self):
+    # display's order as user is building it
+     self.order_text = '\n'.join(self.order)
      self.orderLbl = Label(self.frame1,
-                      text=f"Order:{self.order}")
-     self.orderLbl.grid(row=0,column=5)
+                     text=f"Order{self.order}")
+     self.orderLbl.grid(row=6,column=0)
 
+ # Name entry screen
   def create_frame2_buttons(self):
+    #Enters the user's name for order
     cnfrm = Button(self.frame2,
                    text="Confirm",
                    width=15,
@@ -122,9 +129,25 @@ class App:
                       bd=5,
                       relief=RAISED)
     self.nameInput.grid(row=0,column=2)
-# ===================
-# Event Handling
-# ===================
+   
+ # Order confirmation screen
+  def create_frame3_buttons(self):
+      b2Obtn = Button(self.frame3,
+                      text="Place another order",
+                      width=15,
+                      command=self.b2O)
+      b2Obtn.grid(row=0,column=0, sticky=W)
+
+      endOrder = Button(self.frame3,
+                        text="End Order",
+                        width=15,
+                        command=self.endOrder
+                        )
+      endOrder.grid(row=1,column=0, sticky=W)
+   
+ # ===================
+ # Event Handling
+ # ===================
 
   def submitFunc(self):
     self.frame1.grid_remove()
@@ -132,7 +155,8 @@ class App:
 
   def add2Order(self, item):
     self.order.append(item)
-    self.orderLbl.config(text=f"Order: {self.order}")
+    self.order_text='\n'.join(self.order)
+    self.orderLbl.config(text=f"Order:\n{self.order_text}")
 
   def getName(self):
     name=self.nameInput.get()
@@ -140,13 +164,15 @@ class App:
 
   def cnfrmFunc(self):
     name = self.nameInput.get()
+                
+            
     self.cust_Ords[self.getName().capitalize()]=self.order
     self.order=[]
 
     self.frame2.grid_remove()
     self.nameInput.delete(0, END)
     self.orderLbl.config(text=f"Order: {self.order}")
-    self.frame1.grid()
+    self.frame3.grid()
 
   def endOrder(self):
      self.root.destroy()
@@ -154,10 +180,14 @@ class App:
      self.inv_func()
      self.display_totals()
      self.display_rev()
-
-# ===================
-# Logic
-# ===================
+     
+  def b2O(self):
+      self.frame3.grid_remove()
+      self.frame1.grid()
+     
+ # ===================
+ # Logic
+ # ===================
 
   def calc_totals(self):
     totals = {}
@@ -167,7 +197,7 @@ class App:
             cost += self.menu_dict[item.lower()]
         totals[name] = cost
     return(totals)
-  
+ 
   def calc_rev(self):
       revenue = 0
       for name, total in self.calc_totals().items():
@@ -187,10 +217,10 @@ class App:
                   self.restocks_dict[item.lower()] += 1
       print("\nInventory:")
       for item, count in self.inventory_dict.items():
-          print(f"{item.lower()}: {count}")
+          print(f"{item}: {count}")
       print("\nRestocks:")
       for item, restocks in self.restocks_dict.items():
-          print(f"{item.lower()}: {restocks}")
+          print(f"{item}: {restocks}")
 
   def display_ords(self):
       for name, order in self.cust_Ords.items():
@@ -205,6 +235,5 @@ class App:
     print(f"\nDaily revenue: ${self.calc_rev()}\n")
 
 pos = Tk()
-app = App(pos)
-
+gui = App(pos)
 pos.mainloop()
